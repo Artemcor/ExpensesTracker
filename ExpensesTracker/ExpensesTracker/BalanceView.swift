46dcafc59e244e532f5c8a08680c2cd37c98a919
Artem Stozhok <artem.stozhok@vlogbox.com>
2022-10-01 18:06:05 +0300

    Add subviews to the BalanceView, adjust navigation bar in the BalanceViewContrller

--- ExpensesTracker/ExpensesTracker/BalanceView.swift
+++ ExpensesTracker/ExpensesTracker/BalanceView.swift
@@ -8,37 +8,78 @@
 import UIKit
 
 class BalanceView: UIView {
+    
+    private struct Constants {
+        static let balanceCounterLabelText = "0"
+        static let addToBalanceButtonTitle = "Add"
+        static let addTransactionButtonTitle = "Add transaction"
+    }
 
-    lazy var balanceLabel: UILabel = {
+    // MARK: - Subview variables
+    
+    var balanceCounterLabel: UILabel = {
         let label = UILabel()
         
-        label.text = "Balance"
+        label.text = Constants.balanceCounterLabelText
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
     
+    var addToBalanceButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.setTitle(Constants.addToBalanceButtonTitle, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    var addTransactionButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.setTitle(Constants.addTransactionButtonTitle, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        configureView()
         addSubviews()
         setConstraints()
-        
-        backgroundColor = UIColor.white
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Private
+    
+    private func configureView() {
+        backgroundColor = UIColor.white
+    }
+    
     private func addSubviews() {
-        addSubview(balanceLabel)
+        addSubview(balanceCounterLabel)
+        addSubview(addToBalanceButton)
+        addSubview(addTransactionButton)
     }
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
-            balanceLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-            balanceLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor)
+            balanceCounterLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            balanceCounterLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            
+            addToBalanceButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            addToBalanceButton.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            
+            addTransactionButton.topAnchor.constraint(equalTo: balanceCounterLabel.bottomAnchor, constant: 20),
+            addTransactionButton.centerXAnchor.constraint(equalTo: centerXAnchor)
         ])
     }
 }
--- ExpensesTracker/ExpensesTracker/BalanceViewController.swift
+++ ExpensesTracker/ExpensesTracker/BalanceViewController.swift
@@ -9,15 +9,30 @@ import UIKit
 
 class BalanceViewController: UIViewController {
     
+    private struct Constants {
+        struct NavigationBar {
+            static let title = "Bitcoin balance:"
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
     override func loadView() {
         view = BalanceView()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureNavigatinBar()
     }
-
-
+    
+    // MARK: - Private
+    
+    private func configureNavigatinBar() {
+        guard let navigationController = navigationController else { return }
+        
+        title = Constants.NavigationBar.title
+        navigationController.navigationBar.prefersLargeTitles = true
+    }
 }
-
